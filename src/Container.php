3ca753be90326e0e5627c1da5752ce73254a23e7
lozovoyv <lozovoyv@gmail.com>
2020-12-16 20:19:07 +0300

    Add copyright

--- src/Container.php
+++ src/Container.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the OpxCore.
+ *
+ * Copyright (c) Lozovoy Vyacheslav <opxcore@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace OpxCore\Container;
 
